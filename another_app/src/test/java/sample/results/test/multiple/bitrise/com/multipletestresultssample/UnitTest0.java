7a3217b98e2cdadbfce188456c2b18511f8c909f
trapacska <trapacska@gmail.com>
2018-03-10 14:42:05 +0100

    added multiple unit tests

---
+++ another_app/src/test/java/sample/results/test/multiple/bitrise/com/multipletestresultssample/UnitTest0.java
@@ -0,0 +1,93 @@
+package sample.results.test.multiple.bitrise.com.multipletestresultssample;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class UnitTest0 {
+    @Test
+    public void correctCase0() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void correctCase1() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void correctCase2() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void correctCase3() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void correctCase4() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void correctCase5() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void correctCase6() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void correctCase7() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void correctCase8() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void correctCase9() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void failingCase0() throws Exception {
+        assertEquals(4, 2 + 1);
+    }
+    @Test
+    public void failingCase1() throws Exception {
+        assertEquals(4, 2 + 1);
+    }
+    @Test
+    public void failingCase2() throws Exception {
+        assertEquals(4, 2 + 1);
+    }
+    @Test
+    public void failingCase3() throws Exception {
+        assertEquals(4, 2 + 1);
+    }
+    @Test
+    public void failingCase4() throws Exception {
+        assertEquals(4, 2 + 1);
+    }
+    @Test
+    public void failingCase5() throws Exception {
+        assertEquals(4, 2 + 1);
+    }
+    @Test
+    public void failingCase6() throws Exception {
+        assertEquals(4, 2 + 1);
+    }
+    @Test
+    public void failingCase7() throws Exception {
+        assertEquals(4, 2 + 1);
+    }
+    @Test
+    public void failingCase8() throws Exception {
+        assertEquals(4, 2 + 1);
+    }
+    @Test
+    public void failingCase9() throws Exception {
+        assertEquals(4, 2 + 1);
+    }
+}
---
+++ app/src/androidTest/java/sample/results/test/multiple/bitrise/com/multipletestresultssample/InstrumentedTest0.java
@@ -0,0 +1,99 @@
+package sample.results.test.multiple.bitrise.com.multipletestresultssample;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class InstrumentedTest0 {
+    @Test
+    public void correctUITestCase0() throws Exception {
+        assertEquals(4, 2+2);
+    }
+    @Test
+    public void correctUITestCase1() throws Exception {
+        assertEquals(4, 2+2);
+    }
+    @Test
+    public void correctUITestCase2() throws Exception {
+        assertEquals(4, 2+2);
+    }
+    @Test
+    public void correctUITestCase3() throws Exception {
+        assertEquals(4, 2+2);
+    }
+    @Test
+    public void correctUITestCase4() throws Exception {
+        assertEquals(4, 2+2);
+    }
+    @Test
+    public void correctUITestCase5() throws Exception {
+        assertEquals(4, 2+2);
+    }
+    @Test
+    public void correctUITestCase6() throws Exception {
+        assertEquals(4, 2+2);
+    }
+    @Test
+    public void correctUITestCase7() throws Exception {
+        assertEquals(4, 2+2);
+    }
+    @Test
+    public void correctUITestCase8() throws Exception {
+        assertEquals(4, 2+2);
+    }
+    @Test
+    public void correctUITestCase9() throws Exception {
+        assertEquals(4, 2+2);
+    }
+    @Test
+    public void failedUITestCase0() throws Exception {
+        assertEquals(4, 2+1);
+    }
+    @Test
+    public void failedUITestCase1() throws Exception {
+        assertEquals(4, 2+1);
+    }
+    @Test
+    public void failedUITestCase2() throws Exception {
+        assertEquals(4, 2+1);
+    }
+    @Test
+    public void failedUITestCase3() throws Exception {
+        assertEquals(4, 2+1);
+    }
+    @Test
+    public void failedUITestCase4() throws Exception {
+        assertEquals(4, 2+1);
+    }
+    @Test
+    public void failedUITestCase5() throws Exception {
+        assertEquals(4, 2+1);
+    }
+    @Test
+    public void failedUITestCase6() throws Exception {
+        assertEquals(4, 2+1);
+    }
+    @Test
+    public void failedUITestCase7() throws Exception {
+        assertEquals(4, 2+1);
+    }
+    @Test
+    public void failedUITestCase8() throws Exception {
+        assertEquals(4, 2+1);
+    }
+    @Test
+    public void failedUITestCase9() throws Exception {
+        assertEquals(4, 2+1);
+    }
+}
