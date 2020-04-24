b8efda63fa1aa0ea8d91cff81af1dc4edcaeb0dd
lpusok <7979773+lpusok@users.noreply.github.com>
2020-04-24 16:16:09 +0200

    Tool-1518 nested module sample (#5)
    * Update UnitTest0.java
    
    * Update UnitTest1.java
    
    * Update UnitTest0.java
    
    * Update UnitTest1.java
    
    * Update InstrumentedTest0.java
    
    * Update InstrumentedTest1.java
    
    * add flavors
    
    * add another flavor
    
    * generate code coverage report after uni test run
    
    * only xml jacoc report, into the $buildDir/test-results/testDebugUnitTest output dir
    
    * update jacoco output dir
    
    * Update
    
    * Updating android Gradle plugin and gradle versions, increase compileSdkVersion to 28
    
    * migrate to androidX
    
    * Adding nested module
    
    * Removing cache
    
    * Revert "Merge remote-tracking branch 'origin/code-coverage' into TOOL-1518-nested-module"
    
    This reverts commit 6e5133d9ac0bbd079f31ef7d401815452d3f560c, reversing
    changes made to 9554e639b655ae9dbfa391e46428db87b08f2ea3.
    
    Co-authored-by: Tamas Papik <trapacska@gmail.com>
    Co-authored-by: Akos Birmacher <akos.birmacher@bitrise.io>
    Co-authored-by: Gödrei Krisztián <godreikrisztian@gmail.com>
    Co-authored-by: Gábor Szakács <gaborszakacs@users.noreply.github.com>

--- app/src/androidTest/java/sample/results/test/multiple/bitrise/com/multipletestresultssample/InstrumentedTest1.java
+++ app/src/androidTest/java/sample/results/test/multiple/bitrise/com/multipletestresultssample/InstrumentedTest1.java
@@ -1,8 +1,6 @@
 package sample.results.test.multiple.bitrise.com.multipletestresultssample;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -55,45 +53,5 @@ public class InstrumentedTest1 {
     @Test
     public void correctUITestCase9() throws Exception {
         assertEquals(4, 2+2);
-    }
-    @Test
-    public void failedUITestCase0() throws Exception {
-        assertEquals(4, 2+1);
-    }
-    @Test
-    public void failedUITestCase1() throws Exception {
-        assertEquals(4, 2+1);
-    }
-    @Test
-    public void failedUITestCase2() throws Exception {
-        assertEquals(4, 2+1);
-    }
-    @Test
-    public void failedUITestCase3() throws Exception {
-        assertEquals(4, 2+1);
-    }
-    @Test
-    public void failedUITestCase4() throws Exception {
-        assertEquals(4, 2+1);
-    }
-    @Test
-    public void failedUITestCase5() throws Exception {
-        assertEquals(4, 2+1);
-    }
-    @Test
-    public void failedUITestCase6() throws Exception {
-        assertEquals(4, 2+1);
-    }
-    @Test
-    public void failedUITestCase7() throws Exception {
-        assertEquals(4, 2+1);
-    }
-    @Test
-    public void failedUITestCase8() throws Exception {
-        assertEquals(4, 2+1);
-    }
-    @Test
-    public void failedUITestCase9() throws Exception {
-        assertEquals(4, 2+1);
     }
 }
